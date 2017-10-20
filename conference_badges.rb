effe0afb0a4838522cae55bc62ffa5bd779a5298
Jacqueline Lee <anginblu@gmail.com>
2017-10-20 02:05:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ end
 
 def batch_badge_creator(attendees)
   attendees.each do |name|
-    attendees.index(name)
-    "Hello, my name is #{name}."
+    index = attendees.index(name)
+    attendees(index) = "Hello, my name is #{name}."
   end
 end
