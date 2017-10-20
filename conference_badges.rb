06dc26a2356acb648f83c8e2f71350c63cdc2839
Jacqueline Lee <anginblu@gmail.com>
2017-10-20 02:04:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 def badge_maker (name)
-    puts "Hello, my name is #{name}."
+    "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
   attendees.each do |name|
-    puts "Hello, my name is #{name}."
+    "Hello, my name is #{name}."
   end
 end
