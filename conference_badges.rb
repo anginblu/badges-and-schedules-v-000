74a9060715baf9df5648fd8a1ed8289ea1cce792
Jacqueline Lee <anginblu@gmail.com>
2017-10-20 02:13:06 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,6 +19,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)
+  attendees.each do |name|
+    print batch_badge_creator(attendees)
   assign_rooms(attendees)
 end
